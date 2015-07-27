2f330fc2226f3a0e804b9c17063d5e7b28a71372
Richard Hayes <richardhayes777@gmail.com>
2015-07-27 13:20:36 +0100

    Added off/on functionality

--- lights.sh
+++ lights.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+[[ $1 == on ]] && isOn=true || isOn=false
+
 for i in `seq 3 5`;
 do
-	curl -X PUT -d "{\"on\":$1}" http://192.168.1.65/api/db447f4cbc1d2f31aaa61c1602f297/lights/$i/state
+	json='{"on":'$isOn'}' 
+	echo $json
+	curl -X PUT -d $json  http://192.168.1.65/api/db447f4cbc1d2f31aaa61c1602f297/lights/$i/state
 done
