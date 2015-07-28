63fa4b04821457f6a411fb1f70d66122d0376dda
Zakk Griffiths <gzakkg@gmail.com>
2015-07-28 21:45:56 +0100

    Madness

--- madness.sh
+++ madness.sh
@@ -3,9 +3,10 @@
 lights='python /Users/alexbrown/Zakk/HueScripts/light_script.py'
 
 echo "Let the madness begin!"
+$lights --on
 
 n=1
-while [ $n -lt 100 ]
+while [ $n -lt 10 ]
 do
     $lights --random
     echo "flash $n"
