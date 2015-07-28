302bcb87e817717bf496dc853bb4f435458528ff
Richard Hayes <richardhayes777@gmail.com>
2015-07-28 19:55:33 +0100

    Abstracted logic for interfacing with bridge. Cleaned up and fixed scripts

---
+++ bridge_request.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python
+
+import requests
+import json
+
+def sendRequest(lightNumber,data):
+	headers = {'Content-type': 'application/json'}
+	r = requests.put("http://192.168.1.64/api/1fd93c561b633f071344f0ba3de5301b/lights/"+str(lightNumber)+"/state", data=json.dumps(data), headers=headers)
+	print r.content
+	
+def sendRequests(lightNumbers,data):
+	for lightNumber in lightNumbers:
+		sendRequest(lightNumber,data)
+
+def getLights():
+	r = requests.get("http://192.168.1.64/api/1fd93c561b633f071344f0ba3de5301b/lights/")
+	return r.content
+
+def getStatus(lightNumbers=[]):
+	j = json.loads(getLights())
+	statusDict = {}
+	for key in j.keys():
+		status = j[key]["state"]
+		if not lightNumbers or key in lightNumbers:
+			statusDict[key]=status
+	
+	return statusDict
+
+def isLightOn(lightNumbers=[]):
+	return isAtleastOneInState("on", True, lightNumbers)
+		
+def isLightOff(lightNumbers=[]):
+	return isAtleastOneInState("on", False, lightNumbers)
+	
+def isAllOn(lightNumbers=[]):
+	return isAllInState("on", True, lightNumbers)
+
+def isAllOff(lightNumbers=[]):
+	return isAllInState("on", False, lightNumbers)
+	
+def isAllInState(key, value, lightNumbers):
+	statusDict = getStatus(lightNumbers)
+	for lightNumber in lightNumbers:
+		if not statusDict[lightNumber][key]==value:
+			return False
+			
+	if not lightNumbers:
+		for lightNumber in statusDict.keys():
+			if not statusDict[lightNumber][key]==value:
+				return False
+	return True
+
+def isAtleastOneInState(key, value, lightNumbers=[]):
+	statusDict = getStatus(lightNumbers)
+	for lightNumber in lightNumbers:
+		if statusDict[lightNumber][key]==value:		
+			return True
+			
+	if not lightNumbers:
+		for lightNumber in statusDict.keys():
+			if statusDict[lightNumber][key]==value:
+				return True
+	return False
+	
+
--- light_script.py
+++ light_script.py
@@ -1,72 +1,30 @@
 #!/usr/local/bin/python
 
 import sys
-import re
 import flags
 import requests
 import json
-
-def sendRequest(lightNumber,data):
-	headers = {'Content-type': 'application/json'}
-	r = requests.put("http://192.168.1.64/api/1fd93c561b633f071344f0ba3de5301b/lights/"+str(lightNumber)+"/state", data=json.dumps(data), headers=headers)
-	print r.content
-
-def getLights():
-	r = requests.get("http://192.168.1.64/api/1fd93c561b633f071344f0ba3de5301b/lights/")
-	return r.content
-
-def getStatus(lightNumbers=[]):
-	j = json.loads(getLights())
-	statusArray = []
-	for key in j.keys():
-		status = j[key]["state"]
-		status["lightNumber"]=key
-		if not lightNumbers:
-			statusArray.append(status)
-		elif key in lightNumbers:
-			statusArray.append(status)
-		print key
-	
-	return statusArray
-
-def isLightOn():
-	for status in getStatus():
-		if status["on"]:
-			return True
-	return False
-
-def performSwitch(arg):
-	requestjson={}
-	if arg == "r":
-		for status in getStatus():
-			requestjson["on"]= not status["on"]
-			requestjson["bri"] = status["bri"]
-			requestjson["sat"] = status["sat"]
-			requestjson["hue"] = status["hue"]
-			lightNumber=status["lightNumber"]
-			del status["lightNumber"]
-			sendRequest(lightNumber, requestjson)
-		requestjson={}	
-	elif arg == "e":
-		requestjson["on"]= not isLightOn()
-	return requestjson
+from bridge_request import *
 
 flagPairs = flags.getFlags()
 lights = [3,4,5]
 requestjson={}
+simpleCommands = ["--on","--off","--switch"]
 
 for flagPair in flagPairs:
 	flag = flagPair["flag"]
 	args = flagPair["args"]
 	
-	if flag == "--on":
-		requestjson["on"]=True
+	def checkArgs():
 		if len(args)!=0:
 			lights=args
+	
+	if flag == "--on":
+		requestjson["on"]=True
+		checkArgs()
 	elif flag == "--off":
 		requestjson["on"]=False
-		if len(args)!=0:
-			lights=lights=args
+		checkArgs()
 	elif flag == "-b":
 		requestjson["bri"]=int(args[0])
 	elif flag == "-s":
@@ -76,11 +34,16 @@ for flagPair in flagPairs:
 	elif flag == "-l":
 		lights=args
 	elif flag == "--status":
-		for status in getStatus(args):
-			print status
+		statusDict = getStatus(args)
+		for key in statusDict.keys():
+			print statusDict[key]
 	elif flag == "--switch":
-		requestjson = performSwitch(args[0])
+		requestjson["on"] = not isAllOn()
+		checkArgs()
+		
+	if flag in simpleCommands and len(args)!=0:
+		lights=args
+	
+if requestjson:
+	sendRequests(lights, requestjson)
 
-for lightNumber in lights:
-	if requestjson:
-		sendRequest(lightNumber,requestjson)
