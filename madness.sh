57360bca031ac6de298e3f99ea72f5d1135f18ae
Zakk Griffiths <gzakkg@gmail.com>
2015-08-01 01:54:39 +0100

    Add run_light_scripts.sh

--- madness.sh
+++ madness.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-
 lights='python /Users/alexbrown/Zakk/HueScripts/light_control.py'
 
 $lights --on
 
-#n=1
-#while [ $n -lt 10 ]
-#do
-#    $lights --random
-#    echo "flash $n"
-#    let n+=1
-#done
-#$lights --off
+n=1
+while [ $n -lt 100 ]
+do
+    $lights --random
+    echo "flash $n"
+    let n+=1
+done
+$lights --off
