350d8749f6f7aa1d7601fa50debb95876d30c174
Richard Hayes <richardhayes777@gmail.com>
2015-08-01 11:10:25 +0100

    chain.sh no longer path dependent

--- chain.sh
+++ chain.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 USAGE="Usage: $0 arg1 arg2 arg3 ... argN"
 
-lights="python /Users/alexbrown/Zakk/HueScripts/light_control.py"
+lights="python $DIR/light_control.py"
 
 if [ "$#" == "0" ]; then
     echo $USAGE
@@ -11,7 +13,7 @@ fi
 
 while (( "$#" )); do
     echo; echo "Running: lights $1"
-    $lights $1  &
+    $lights $1  
     sleep 2; echo
     shift
 done
