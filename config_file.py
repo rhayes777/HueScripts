b9f126412cd6f745f769a780de2c1e23ba54dc23
Richard Hayes <richardhayes777@gmail.com>
2015-08-12 17:27:06 +0100

    Added full path, config now works immediately

--- config_file.py
+++ config_file.py
@@ -1,23 +1,28 @@
 import json
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+path = os.path.join(__location__, 'config.json')
 
 def addState(name, state):
-	with open("config.json", 'w+') as f:
+	with open(path, 'w+') as f:
 		jsonContents = loadContents(f)
 		print jsonContents
 		jsonContents[name]=state
 		f.write(json.dumps(jsonContents))
 		
 def loadStates():
-	with open("config.json", 'r') as f:
+	with open(path, 'r') as f:
 		return loadContents(f)
 
 def loadConfig():
-	with open("config.json", 'r') as f:
+	with open(path, 'r') as f:
 		jsonContents = loadContents(f)
 		return jsonContents["ip"], jsonContents["username"]
 
 def saveConfig(ip, username):
-	with open("config.json", 'w+') as f:
+	with open(path, 'w+') as f:
 	        jsonContents = loadContents(f)
 	        jsonContents["ip"]=ip
 	        jsonContents["username"]=username
