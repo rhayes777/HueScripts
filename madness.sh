1ff17287c4a08ca7b9b3e35fff36613464c013d4
Richard Hayes <richardhayes777@gmail.com>
2015-08-20 23:11:37 +0100

    Fixed madness

--- madness.sh
+++ madness.sh
@@ -2,13 +2,13 @@
 source path_finder.py
 
 
-$lights --on
+lights on
 
 n=1
 while [ $n -lt 100 ]
 do
-    $lights --random
+    lights random
     echo "flash $n"
     let n+=1
 done
-$lights --off
+lights off
