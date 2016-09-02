573c49acd1dcd3d0c24c35c2fc474dbc0ef1a83d
Richard <richardhayes777@gmail.com>
2016-09-02 17:40:19 +0100

    added light clokc

--- light_clock.py
+++ light_clock.py
@@ -10,11 +10,13 @@ def set_lights_by_time():
     minute = time.minute
     hour = time.hour
 
+    print "second, {}, minute {}, hour {}".format(second, minute, hour)
+
     minute_color = int(round(65535 * second / 60))
     hour_color = int(round(65535 * (minute + second / 60) / 60))
     day_color = int(round(65535 * (hour + minute / 60 + second / 3600) / 24))
 
-    print "minute_color {}, hour_color {}, day_color {}".format(minute_color, hour_color, day_color)
+    # print "minute_color {}, hour_color {}, day_color {}".format(minute_color, hour_color, day_color)
 
     sendColorRequestForHue(lights[0], minute_color)
     sendColorRequestForHue(lights[1], hour_color)
