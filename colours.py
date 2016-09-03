33e854f586715fff2b105c0b0c9fea9409a96ae3
Richard <richardhayes777@gmail.com>
2016-09-03 19:56:34 +0100

    icon and temp work but rain intensity may be wrong

--- colours.py
+++ colours.py
@@ -1,51 +1,64 @@
 #!/usr/local/bin/python
 from random import randint
-import colorsys 
+import colorsys
 
 HUE_MAX = 65535
 BRI_MAX = 255
 SAT_MAX = BRI_MAX
 HUE_MIN = 0
 
+
 def colourForHBS(hue, brightness, saturation):
-	return {"bri":brightness,"sat":saturation,"hue":hue}
-	
+    return {"bri": brightness, "sat": saturation, "hue": hue}
+
+
 def colourForRGB(red, green, blue):
-	hue, brightness, saturation = colorsys.rgb_to_hls(red, green, blue)
-	return colourForHBS(int(hue * HUE_MAX), int(brightness * BRI_MAX), int(saturation * SAT_MAX))
+    hue, brightness, saturation = colorsys.rgb_to_hls(red, green, blue)
+    return colourForHBS(int(hue * HUE_MAX), int(brightness * BRI_MAX), int(saturation * SAT_MAX))
+
 
 def randomColour():
-	return colourForHBS(randint(0,HUE_MAX), randint(0,BRI_MAX), randint(0,SAT_MAX))
-	
+    return colourForHBS(randint(0, HUE_MAX), randint(0, BRI_MAX), randint(0, SAT_MAX))
+
+
 def randomFullColour():
-	return colourForHBS(randint(0,HUE_MAX), BRI_MAX, SAT_MAX)
-	
+    return colourForHBS(randint(0, HUE_MAX), BRI_MAX, SAT_MAX)
+
+
 def whiteColour():
-	return colourForHBS(0, 255, 0)
+    return colourForHBS(0, 255, 0)
+
 
 def colourForHue(hue):
-	return colourForHBS(hue, 255, 255)
+    return colourForHBS(hue, 255, 255)
+
 
 def colourForHueDegrees(hueDegrees):
-	return colourForHue(HUE_MAX*hueDegrees/360)
-	
-colourDict = {"red":colourForHueDegrees(0),"orange":colourForHueDegrees(40),"yellow":colourForHueDegrees(80),"sky":colourForHueDegrees(200),"purple":colourForHueDegrees(280),"pink":colourForHueDegrees(300), "green":colourForHueDegrees(130), "blue":colourForHueDegrees(240),"turquoise":colourForHueDegrees(180),"red2":colourForHueDegrees(360), "light": whiteColour()}
-	
+    return colourForHue(HUE_MAX * hueDegrees / 360)
+
+
+colourDict = {"red": colourForHueDegrees(0), "orange": colourForHueDegrees(40), "yellow": colourForHueDegrees(80),
+              "sky": colourForHueDegrees(200), "purple": colourForHueDegrees(280), "pink": colourForHueDegrees(300),
+              "green": colourForHueDegrees(130), "blue": colourForHueDegrees(240),
+              "turquoise": colourForHueDegrees(180), "red2": colourForHueDegrees(360), "light": whiteColour(),
+              "white": whiteColour()}
+
+
 def getColour(name):
-	return colourDict[name]
-	
+    return colourDict[name]
+
+
 def getHue(name):
-	return getColour(name)["hue"]
-	
+    return getColour(name)["hue"]
+
+
 def randomColourInRange(min, max):
-	return colourForHue(randint(min, max))
-	
+    return colourForHue(randint(min, max))
+
+
 def isOverHueMax(colour):
-	return colour["hue"]>HUE_MAX
-	
+    return colour["hue"] > HUE_MAX
+
+
 def isUnderHueMin(colour):
-	return colour["hue"]<HUE_MIN
-
-	
-
-
+    return colour["hue"] < HUE_MIN
--- weather.py
+++ weather.py
@@ -1,13 +1,44 @@
-
+from datetime import datetime, timedelta
 import urllib
-
-
-import os
-import socket
-
 import json
-
 from urllib2 import urlopen
+import colours
+from bridge_request import sendColorRequest
+from light_control import lights
+
+
+CLEAR_DAY = "clear-day"
+CLEAR_NIGHT = "clear-night"
+RAIN = "rain"
+SNOW = "snow"
+SLEET = "sleet"
+WIND = "wind"
+FOG = "fog"
+CLOUDY = "cloudy"
+PARTLY_CLOUDY_DAY = "partly-cloudy-day"
+PARTLY_CLOUDY_NIGHT = "partly-cloudy-night"
+
+
+class WeatherColor:
+
+    def __init__(self, icon, color):
+        self.icon = icon
+        self.color = colours.colourDict[color]
+
+
+weather_colors = \
+    [
+        WeatherColor(CLEAR_DAY, "sky"),
+        WeatherColor(CLEAR_NIGHT, "sky"),
+        WeatherColor(RAIN, "blue"),
+        WeatherColor(SNOW, "blue"),
+        WeatherColor(SLEET, "blue"),
+        WeatherColor(FOG, "white"),
+        WeatherColor(CLOUDY, "white"),
+        WeatherColor(PARTLY_CLOUDY_DAY, "white"),
+        WeatherColor(PARTLY_CLOUDY_NIGHT, "white")
+        ]
+
 
 def set_lights_by_weather():
 
@@ -16,14 +47,50 @@ def set_lights_by_weather():
 
     url = 'https://freegeoip.net/json/{}'.format(ip)
 
-    response = json.loads(urllib.urlopen(url).read())
+    location_json = json.loads(urllib.urlopen(url).read())
 
-    lat = response["latitude"]
-    lng = response["longitude"]
+    lat = location_json["latitude"]
+    lng = location_json["longitude"]
 
-    forecast = json.loads(urlopen("https://api.forecast.io/forecast/2beeada32fb06ea94f0790895259d23c/{},{}".format(lat, lng)).read())
+    weather_json = json.loads(urlopen("https://api.forecast.io/forecast/2beeada32fb06ea94f0790895259d23c/{},{}".format(lat, lng)).read())
 
-    print forecast
+    daily_forecasts = weather_json["daily"]["data"]
+
+    tomorrow = (datetime.now() + timedelta(days=1)).date()
+
+    for day_forecast in daily_forecasts:
+        if (datetime.utcfromtimestamp(day_forecast["time"])).date() == tomorrow:
+            temperature = (day_forecast["temperatureMax"] + day_forecast["temperatureMin"]) / 2
+            precip_intensity = day_forecast["precipIntensity"]
+            icon = day_forecast["icon"]
+
+            print temperature
+
+            temp_color_degrees = int((100 - temperature) * 2.4)
+
+            if temp_color_degrees > 240:
+                temp_color_degrees = 240
+            elif temp_color_degrees < 0:
+                temp_color_degrees = 0
+
+            precip_brightness = int(255 * precip_intensity / 75)
+
+            temp_color = colours.colourForHueDegrees(temp_color_degrees)
+            icon_color = filter(lambda weather_color: weather_color.icon == icon, weather_colors)[0].color
+            precip_color = colours.colourDict["blue"]
+            precip_color["bri"] = precip_brightness
+
+            sendColorRequest(lights[0], temp_color)
+            sendColorRequest(lights[1], icon_color)
+            sendColorRequest(lights[2], precip_color)
+
+            print "precip_brightness = {}".format(precip_brightness)
+            print "temp_color_degrees = {}".format(temp_color_degrees)
+            print "icon_color = {}".format(icon_color)
+            print temp_color
+            print  precip_color
+
+    # datetime.utcfromtimestamp()
 
 
 if __name__ == "__main__":
